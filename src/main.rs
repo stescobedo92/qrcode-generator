d48f7784bfd1fad0a9d2898b9be58c48c0f761ce
Sergio Triana Escobedo <stescobedo.31@gmail.com>
2024-03-19 10:14:07 -0600

    Refactor code

--- src/main.rs
+++ src/main.rs
@@ -2,19 +2,26 @@ use qrcode::QrCode;
 use image::Luma;
 use std::env;
 
-fn main(){
-    let args: Vec<String> = env::args().collect();
-
+fn parse_arguments(args: Vec<String>) {
     //Verify that there are at least two arguments (the command and the text to convert).
     if args.len() < 3 {
         println!("Usage: {} <text to convert> <output path>", args[0]);
         return;
     }
 
-    let text_to_convert = args[1].as_bytes();
+    let text_to_convert = &args[1];
     let output_path = &args[2];
-    let code = QrCode::new(text_to_convert).unwrap();
 
+    generate_qr_code(text_to_convert, output_path);
+}
+
+fn generate_qr_code(text: &str, output_path: &str) {
+    let code = QrCode::new(text.as_bytes()).unwrap();
     let image = code.render::<Luma<u8>>().build();
     image.save(output_path).unwrap();
+}
+
+fn main(){
+    let args: Vec<String> = env::args().collect();
+    parse_arguments(args);
 }
