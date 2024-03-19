00d6401ce7b860c639e039da2d21e0871d43c1fc
Sergio Triana Escobedo <stescobedo.31@gmail.com>
2024-03-19 10:08:45 -0600

    Adding code for generate qr code according text passed by parameter

--- src/main.rs
+++ src/main.rs
@@ -1,2 +1,20 @@
+use qrcode::QrCode;
+use image::Luma;
+use std::env;
+
 fn main(){
+    let args: Vec<String> = env::args().collect();
+
+    //Verify that there are at least two arguments (the command and the text to convert).
+    if args.len() < 3 {
+        println!("Usage: {} <text to convert> <output path>", args[0]);
+        return;
+    }
+
+    let text_to_convert = args[1].as_bytes();
+    let output_path = &args[2];
+    let code = QrCode::new(text_to_convert).unwrap();
+
+    let image = code.render::<Luma<u8>>().build();
+    image.save(output_path).unwrap();
 }
